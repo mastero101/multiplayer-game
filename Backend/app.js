0697fbed1b78c71b8ba97e7cb0acba21a0899b4a
mastero101 <mastershift_101@hotmail.com>
2024-07-26 00:41:32 -0600

    Actualizado Backend para poder usarse con Vercel

--- Backend/app.js
+++ Backend/app.js
@@ -25,6 +25,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const PORT = process.env.PORT || 5000;
 
+// Export the app for Vercel to use
+module.exports = app;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
